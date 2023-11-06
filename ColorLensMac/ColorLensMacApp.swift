3a62001324b22bf06d28e34e12843af499f6ec78
dimpurr <dimpur@live.com>
2023-11-06 18:33:47 +0000

    Initial Commit

---
+++ ColorLensMac/ColorLensMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorLensMacApp.swift
+//  ColorLensMac
+//
+//  Created by Cheny Dimpurr on 06/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorLensMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorLensMac/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ColorLensMac
+//
+//  Created by Cheny Dimpurr on 06/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
